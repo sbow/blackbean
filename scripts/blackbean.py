19157578343ed6cf7197da7d8c6616aeb7c804f3
Shaun Bowman <shaun_bowman@hotmail.com>
2019-09-17 23:52:50 -0400

    fu

--- scripts/blackbean.py
+++ scripts/blackbean.py
@@ -23,6 +23,7 @@ PIN_DC = 24
 PIN_RST = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
+disp = None
 
 # Solid State Relay and RGB LED Setup
 from gpiozero import RGBLED, LED
@@ -53,6 +54,34 @@ def RelayDemo(self):
 	relay.blink(1)
 	sleep(10)
 	
+def TftDemo(self):
+	# Create TFT LCD display class.
+	disp = TFT.ST7789(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
+
+	# Initialize display.
+	disp.begin()
+
+	# Clear the display to a red background.
+	# Can pass any tuple of red, green, blue values (from 0 to 255 each).
+	disp.clear((255, 0, 0))
+
+	# Get a PIL Draw object to start drawing on the display buffer.
+	draw = disp.draw()
+
+	# Draw a cyan triangle with a black outline.
+	draw.polygon([(10, 275), (110, 240), (110, 310)], outline=(0,0,0), fill=(0,255,255))
+
+	# Load default font.
+	font = ImageFont.load_default()	
+	
+	# Write two lines of white text on the buffer, rotated 90 degrees counter clockwise.
+	draw_rotated_text(disp.buffer, 'Hello World!', (150, 120), 90, font, fill=(255,255,255))
+	draw_rotated_text(disp.buffer, 'This is a line of text.', (170, 90), 90, font, fill=(255,255,255))
+
+	# Write buffer to display hardware, must be called to make things visible on the
+	# display!
+	disp.display()	
+
 def RelayOn(self):
 	relay.On()
 
@@ -65,3 +94,20 @@ def LedColor(self, rgb=(1,1,1))
 def DbConnect(self):
 	con = sqlite3.connect(DATABASE) 
 	cursorObj = con.cursor()
+
+# Define a function to create rotated text.  Unfortunately PIL doesn't have good
+# native support for rotated fonts, but this function can be used to make a
+# text image and rotate it so it's easy to paste in the buffer.
+def draw_rotated_text(image, text, position, angle, font, fill=(255,255,255)):
+    # Get rendered font width and height.
+    draw = ImageDraw.Draw(image)
+    width, height = draw.textsize(text, font=font)
+    # Create a new image with transparent background to store the text.
+    textimage = Image.new('RGBA', (width, height), (0,0,0,0))
+    # Render the text.
+    textdraw = ImageDraw.Draw(textimage)
+    textdraw.text((0,0), text, font=font, fill=fill)
+    # Rotate the text image.
+    rotated = textimage.rotate(angle, expand=1)
+    # Paste the text into the image, using it as a mask for transparency.
+    image.paste(rotated, position, rotated)
